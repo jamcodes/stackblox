35eb8610af67eec0d7c78a9d3a60dd5a8e7b9251
Steve Robinson <ssrobins@gmail.com>
2019-11-24 10:32:33 -0800

    Add Xcode folder reference so iOS app submission can be fully automated

--- build_ios.sh
+++ build_ios.sh
@@ -12,4 +12,10 @@ conan install --update .. -s os=iOS -s arch=armv7 -s os.version=8.0 -s compiler.
 
 cmake -G Xcode -DCMAKE_SYSTEM_NAME=iOS -DCMAKE_OSX_ARCHITECTURES="armv7 arm64" -DCMAKE_OSX_DEPLOYMENT_TARGET=8.0 -DCMAKE_TRY_COMPILE_TARGET_TYPE=STATIC_LIBRARY ..
 
+python3 ../add_xcode_folder_reference.py --project=/Users/steve/Code/stackblox/build_ios/StackBlox.xcodeproj/project.pbxproj --folderPath=src/StackBlox.xcassets --target=StackBlox
+python3 ../add_xcode_folder_reference.py --project=/Users/steve/Code/stackblox/build_ios/StackBlox.xcodeproj/project.pbxproj --folderPath=src/assets --target=StackBlox
+
 cmake --build . --config Release
+
+xcodebuild -workspace StackBlox.xcodeproj/project.xcworkspace -scheme StackBlox archive -archivePath StackBlox.xcarchive
+xcodebuild -exportArchive -archivePath StackBlox.xcarchive -exportOptionsPlist ../exportOptions.plist -exportPath .
